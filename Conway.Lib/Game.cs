880d3ce67addbf6311875c018354533c696c88a6
logan <logan.e.miles@gmail.com>
2020-04-12 13:32:32 -0600

    Can initialize board and set a cell's value

--- Conway.Lib/Game.cs
+++ Conway.Lib/Game.cs
@@ -6,6 +6,23 @@ namespace Conway.Lib {
 
         public Game(int height, int width) {
             board = new Cell[height, width];
+            Initialize();
+        }
+
+        private void Initialize() {
+            for (int i = 0; i < board.GetLength(0); i++) {
+                for (int j = 0; j < board.GetLength(1); j++) {
+                    board[i, j] = new Cell();
+                }
+            }
+        }
+
+        public void SetCell(int row, int column, bool value) {
+            board[row, column].IsAlive = value;
+        }
+
+        public Cell[, ] GetStatus() {
+            return board;
         }
     }
 }
--- Conway.Tests/GameTests.cs
+++ Conway.Tests/GameTests.cs
@@ -1,11 +1,16 @@
 using System;
+using Conway.Lib;
+using FluentAssertions;
 using Xunit;
 
 namespace Conway.Tests {
     public class GameTests {
         [Fact]
-        public void Test1() {
-
+        public void SetCell_True_ShouldBeAlive() {
+            Game g = new Game(1, 1);
+            g.GetStatus()[0, 0].IsAlive.Should().Be(false);
+            g.SetCell(0, 0, true);
+            g.GetStatus()[0, 0].IsAlive.Should().Be(true);
         }
     }
 }
