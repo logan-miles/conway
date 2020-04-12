0df3f2c3a60abdb2d75faf52003b0411da75a1d9
logan <logan.e.miles@gmail.com>
2020-04-12 12:54:00 -0600

    For realsies

---
+++ Conway.Lib/Game.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Conway.Lib {
+    public class Game {}
+}
---
+++ Conway.Tests/GameTests.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace Conway.Tests {
+    public class GameTests {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
